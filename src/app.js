1bedcba7e22bc1e789a76e0ce1fa15919981fc8b
Rupak kumar <rupak9540@gmail.com>
2022-01-22 22:49:50 +0530

    adding heroku in script of pakage.json

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const hbs = require('hbs');
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 
 const staticpath = path.join(__dirname,"../public");
